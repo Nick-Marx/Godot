22ce57a26e4585cf9dec19013cf4c51fb9f6af01
Nick-Marx <ctrlaltsmite@gmail.com>
2022-09-19 10:00:45 -0500

    updated player script
    changed physics to physics_process
    setup a fn to call to change rotation direction
    added 2nd collision chape to player; hopefully i can use it to better detect collisions

--- Pivot/Script/pivot_player.gd
+++ Pivot/Script/pivot_player.gd
@@ -2,17 +2,28 @@ extends KinematicBody
 
 
 # Declare member variables here.
-export var speed = .05
+export var speed = 0.5
+#keep track of rotation direction. false = clockwise
+var rotationDirection = false
 
+#fn for switching rotation direction
+func rotation_direction_change():
+	#change speed variable based on rotationDirection
+	if rotationDirection == false:
+		speed *= -1
+		rotationDirection = true
+	else:
+		speed *= -1
+		rotationDirection = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	rotation_direction_change()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	#constantly rotates player. start clockwise
-	rotate_z(-speed)
+	rotate_z(speed)
 	
 	#if btn pressed and dot collider overlap -> get dot pos, move player to dot pos, flip player, change rot direction
 	#if Input.is_action_pressed("action_one") and :
