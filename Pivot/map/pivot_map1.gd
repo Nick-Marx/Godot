2612def243ea974b8b6f6c6845397e6f41edbbbe
Nick-Marx <ctrlaltsmite@gmail.com>
2022-09-25 08:20:59 -0500

    update
    created dot focused branch to test dot or player focused movement

--- Pivot/map/pivot_map1.gd
+++ Pivot/map/pivot_map1.gd
@@ -1,9 +1,18 @@
 extends Node3D
 
 
+#@onready var pp = $pivot_player #holds pivot_player node
+
+#var dotCounter:int = 0
+
+#func build_map(pp, dC):
+	
+	
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#build_map(pp, dotCounter)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Pivot/player/pivot_player.gd
+++ Pivot/player/pivot_player.gd
@@ -2,11 +2,10 @@ extends CharacterBody3D
 
 
 @export var speed:float = -0.0125
+const spdCtrl:int = 4
+const rotDir:int = -1
 
 
-
-func change_direction():
-	speed *= -1
 
 func _physics_process(delta):
 	rotate_z(speed)
