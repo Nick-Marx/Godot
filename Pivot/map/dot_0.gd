8c7c0511ea2ba62e1f1028579680d3d46f3983b5
Nick-Marx <ctrlaltsmite@gmail.com>
2022-09-25 10:45:01 -0500

    update
    success!
    using player script to control everything worked. i think i could have gotten it working in the dot script, but this way makes more sense anyways

--- Pivot/map/dot_0.gd
+++ Pivot/map/dot_0.gd
@@ -1,76 +1,72 @@
 extends StaticBody3D
 
 
-class_name dot_0
-var isTouchOuter: bool = false #trigger for when outer area is colliding
-var isTouchInner: bool = false #trigger for when inner area is colliding
-var isActive: bool = false #trigger for when dot shares position with player
-var pp #will hold player node
-
-
-
-#moves player to new dot if player uses action only in outer trigger zone
-func outer_area_player_action():
-	if Input.is_action_pressed("action_one"):
-		pp.global_position = global_position
-		pp.rotate_z(PI)
-		if (pp.speed < -0.02) or (pp.speed > 0.02):
-			pp.speed /= pp.spdCtrl * pp.rotDir
-		else:
-			pp.speed *= pp.rotDir #sets player speed to low and changes direction
-		isActive = true
-		print(pp.global_position)
-		await _on_area_3d_outer_body_exited(pp)
-		
-#moves player to new dot if player uses action in inner trigger area and speeds rotation
-func inner_area_player_action():
-	if Input.is_action_just_pressed("action_one"):
-		pp.global_position = global_position
-		pp.rotate_z(PI)
-		if (pp.speed > -0.05) and (pp.speed < 0.05):
-			pp.speed *= pp.spdCtrl * pp.rotDir
-		else:
-			pp.speed *= pp.rotDir #sets player speed to low and changes direction
-		isActive = true
-		print(pp.global_position)
-		await _on_area_3d_inner_body_exited(pp)
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pp = get_node("/root/pivot_map1/pivot_player")
-	print(name, "isActive= ", isActive)
-	print(global_position)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	if pp.global_position == global_position:
-		isActive = true
-	else:
-		isActive = false
-	
-	if isTouchInner == true and isActive == false:
-		inner_area_player_action()
-	elif isTouchOuter == true and isTouchInner != true and isActive == false:
-		outer_area_player_action()
-
-
-func _on_area_3d_outer_body_entered(body):
-	if body == pp:
-		isTouchOuter = true
-	print(name, " isTouchOuter= ", isTouchOuter)
-	print(body)
-	print(name, "isActive= ", isActive)
-
-func _on_area_3d_outer_body_exited(body):
-	if body == pp:
-		isTouchOuter = false
-	print("isTouchOuter= ", isTouchOuter)
-
-func _on_area_3d_inner_body_entered(body):
-	if body == pp:
-		isTouchInner = true
-
-func _on_area_3d_inner_body_exited(body):
-	if body == pp:
-		isTouchInner = false
+#class_name dot_0
+#
+#
+#
+##moves player to new dot if player uses action only in outer trigger zone
+#func outer_area_player_action():
+#	if Input.is_action_pressed("action_one"):
+#		pp.global_position = global_position
+#		pp.rotate_z(PI)
+#		if (pp.speed < -0.02) or (pp.speed > 0.02):
+#			pp.speed /= pp.spdCtrl * pp.rotDir
+#		else:
+#			pp.speed *= pp.rotDir #sets player speed to low and changes direction
+#		isActive = true
+#		print(pp.global_position)
+#		await _on_area_3d_outer_body_exited(pp)
+#
+##moves player to new dot if player uses action in inner trigger area and speeds rotation
+#func inner_area_player_action():
+#	if Input.is_action_just_pressed("action_one"):
+#		pp.global_position = global_position
+#		pp.rotate_z(PI)
+#		if (pp.speed > -0.05) and (pp.speed < 0.05):
+#			pp.speed *= pp.spdCtrl * pp.rotDir
+#		else:
+#			pp.speed *= pp.rotDir #sets player speed to low and changes direction
+#		isActive = true
+#		print(pp.global_position)
+#		await _on_area_3d_inner_body_exited(pp)
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pp = get_node("/root/pivot_map1/pivot_player")
+#	print(name, "isActive= ", isActive)
+#	print(global_position)
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _physics_process(delta):
+#	if pp.global_position == global_position:
+#		isActive = true
+#	else:
+#		isActive = false
+#
+#	if isTouchInner == true and isActive == false:
+#		inner_area_player_action()
+#	elif isTouchOuter == true and isTouchInner != true and isActive == false:
+#		outer_area_player_action()
+#
+#
+#func _on_area_3d_outer_body_entered(body):
+#	if body == pp:
+#		isTouchOuter = true
+#	print(name, " isTouchOuter= ", isTouchOuter)
+#	print(body)
+#	print(name, "isActive= ", isActive)
+#
+#func _on_area_3d_outer_body_exited(body):
+#	if body == pp:
+#		isTouchOuter = false
+#	print("isTouchOuter= ", isTouchOuter)
+#
+#func _on_area_3d_inner_body_entered(body):
+#	if body == pp:
+#		isTouchInner = true
+#
+#func _on_area_3d_inner_body_exited(body):
+#	if body == pp:
+#		isTouchInner = false
--- Pivot/player/pivot_player.gd
+++ Pivot/player/pivot_player.gd
@@ -2,10 +2,73 @@ extends CharacterBody3D
 
 
 @export var speed:float = -0.0125
-const spdCtrl:int = 4
+var hiSpd:float = 0.05
+var loSpd:float = 0.0125
 const rotDir:int = -1
+var isActive:bool = false
+var innerActive:bool = false
+var activeBody
+var prevDotPos
 
 
+func _ready():
+	prevDotPos = global_position
+	print(name)
+
+func change_rotation_dir():
+	hiSpd *= rotDir
+	loSpd *= rotDir
 
 func _physics_process(delta):
 	rotate_z(speed)
+	
+	if innerActive:
+		inner_dot_action_press(activeBody)
+		isActive = false
+	if isActive and !innerActive:
+		outer_dot_action_press(activeBody)
+		isActive = false
+
+
+	
+func outer_dot_action_press(body):
+	if body.global_position == prevDotPos:
+		return
+	if Input.is_action_just_pressed("action_one") or Input.is_action_pressed("action_one"):
+		global_position = body.global_position
+		rotate_z(PI)
+		change_rotation_dir()
+		speed = loSpd #sets player speed to low and changes direction
+		print("outer: ", global_position)
+	
+func inner_dot_action_press(body):
+	if body.global_position == prevDotPos:
+		return
+	if Input.is_action_just_pressed("action_one"):
+		global_position = body.global_position
+		rotate_z(PI)
+		change_rotation_dir()
+		speed = hiSpd #sets player speed to low and changes direction
+		print("inner: ", global_position)
+	
+
+func _on_area_3d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if body.name == name:
+		return
+	if body_shape_index == 0:
+		isActive = true
+		activeBody = body
+	if body_shape_index == 1:
+		innerActive = true
+		
+#	print(body_rid, "\n", body, "\n", body_shape_index, "\n", local_shape_index, "\n", "\n")
+
+
+func _on_area_3d_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
+	if body_shape_index == 1:
+		innerActive = false
+	if local_shape_index == 0:
+		isActive = false
+		activeBody = null
+	if body_shape_index == 0 and body.global_position == prevDotPos:
+		prevDotPos = global_position
